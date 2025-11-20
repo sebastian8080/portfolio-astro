09c72a528673b9118b2ee4f8601914cecad3f9dc
sebastian8080 <sebas25211@hotmail.com>
2025-11-20 16:16:16 -0500

    feat: add sitemap and robots files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [tailwind({
-    // Desactiva la integración automática
-    applyBaseStyles: false,
-  })],
+  site: "https://sebasarmijos.dev",
+  integrations: [
+    tailwind({
+      // Desactiva la integración automática
+      applyBaseStyles: false,
+    }),
+  ],
 });
